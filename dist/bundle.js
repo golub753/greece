a5630b854c7b0e9533425f496d5cc62e291e75f8
Anton Holub <golubanton753@gmail.com>
2023-02-17 15:50:47 +0300

    feat: create info section

--- dist/bundle.js
+++ dist/bundle.js
@@ -3,29 +3,51 @@
 var __webpack_exports__ = {};
 
 $(document).ready(function () {
-  var modal = document.querySelector('.modal'),
-    overlay = document.querySelector('.overlay');
-  function openModal() {
-    var btns = document.querySelectorAll('.btn');
-    btns.forEach(function (btn) {
-      btn.addEventListener('click', function () {
-        var title = btn.getAttribute('data-title');
-        if (title) {
-          modal.querySelector('.modal-subtitle').textContent = title;
-        }
-        modal.classList.add('active');
-        overlay.classList.add('active');
+  // const modal = document.querySelector('.modal'),
+  //     overlay = document.querySelector('.overlay');
+
+  // function openModal() {
+  //     const btns = document.querySelectorAll('.btn');
+  //     btns.forEach((btn) => {
+  //         btn.addEventListener('click', () => {
+  //             const title = btn.getAttribute('data-title');
+  //             if (title) {
+  //                 modal.querySelector('.modal-subtitle').textContent = title;
+  //             }
+  //             modal.classList.add('active');
+  //             overlay.classList.add('active');
+  //         });
+  //     });
+  // }
+
+  // function closeModal() {
+  //     overlay.addEventListener('click', () => {
+  //         modal.classList.remove('active');
+  //         overlay.classList.remove('active');
+  //     });
+  // }
+
+  function switchTabs() {
+    var tabs = document.querySelectorAll('.info-right-stoun-tab'),
+      contents = document.querySelectorAll('.info-right-stoun-content');
+    tabs.forEach(function (tab) {
+      tab.addEventListener('click', function () {
+        var id = tab.getAttribute('data-target');
+        tabs.forEach(function (item) {
+          item.classList.remove('active');
+        });
+        tab.classList.add('active');
+        contents.forEach(function (item) {
+          item.classList.remove('active');
+        });
+        document.querySelector(id).classList.add('active');
       });
     });
   }
-  function closeModal() {
-    overlay.addEventListener('click', function () {
-      modal.classList.remove('active');
-      overlay.classList.remove('active');
-    });
-  }
-  openModal();
-  closeModal();
+
+  // openModal();
+  // closeModal();
+  switchTabs();
 });
 /******/ })()
 ;
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,30 +1,50 @@
 import '../sass/style.scss';
 
 $(document).ready(() => {
-    const modal = document.querySelector('.modal'),
-        overlay = document.querySelector('.overlay');
+    // const modal = document.querySelector('.modal'),
+    //     overlay = document.querySelector('.overlay');
 
-    function openModal() {
-        const btns = document.querySelectorAll('.btn');
-        btns.forEach((btn) => {
-            btn.addEventListener('click', () => {
-                const title = btn.getAttribute('data-title');
-                if (title) {
-                    modal.querySelector('.modal-subtitle').textContent = title;
-                }
-                modal.classList.add('active');
-                overlay.classList.add('active');
+    // function openModal() {
+    //     const btns = document.querySelectorAll('.btn');
+    //     btns.forEach((btn) => {
+    //         btn.addEventListener('click', () => {
+    //             const title = btn.getAttribute('data-title');
+    //             if (title) {
+    //                 modal.querySelector('.modal-subtitle').textContent = title;
+    //             }
+    //             modal.classList.add('active');
+    //             overlay.classList.add('active');
+    //         });
+    //     });
+    // }
+
+    // function closeModal() {
+    //     overlay.addEventListener('click', () => {
+    //         modal.classList.remove('active');
+    //         overlay.classList.remove('active');
+    //     });
+    // }
+
+    function switchTabs() {
+        const tabs = document.querySelectorAll('.info-right-stoun-tab'),
+            contents = document.querySelectorAll('.info-right-stoun-content');
+
+        tabs.forEach((tab) => {
+            tab.addEventListener('click', () => {
+                const id = tab.getAttribute('data-target');
+                tabs.forEach((item) => {
+                    item.classList.remove('active');
+                });
+                tab.classList.add('active');
+                contents.forEach((item) => {
+                    item.classList.remove('active');
+                });
+                document.querySelector(id).classList.add('active');
             });
         });
     }
 
-    function closeModal() {
-        overlay.addEventListener('click', () => {
-            modal.classList.remove('active');
-            overlay.classList.remove('active');
-        });
-    }
-
-    openModal();
-    closeModal();
+    // openModal();
+    // closeModal();
+    switchTabs();
 });
